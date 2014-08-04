fb3206fa69613f15009ea90d613f722af7459ddc
Mark Sta Ana <booyaabooyaabooyaa@gmail.com>
2014-08-04 17:47:23 +0100

    fixed typo in script generation

--- databases/oracle/create_insert_sproc.sql
+++ databases/oracle/create_insert_sproc.sql
@@ -1,11 +1,8 @@
--- description: pl/sql script to generate insert sproc for a given table
--- usage: sqlplus conn/to/your/oracle @create_insert_sproc TABLE_NAME (SPROC_PREFIX optional)
--- reqs: you have data dictionary access or gtfo
 SET SERVEROUTPUT ON
 SET VERIFY OFF
 DECLARE  
   v_table_name VARCHAR2(30) := '&1';
-  v_sproc_prefix VARCHAR2(5) := '&2';
+  v_sproc_prefix VARCHAR2(15) := '&2';
   
   CURSOR c_cursor IS
     SELECT *
@@ -25,7 +22,7 @@ DECLARE
 BEGIN
   SELECT MAX(column_id) INTO v_last_column_id FROM user_tab_columns WHERE table_name = v_table_name;
   
-  
+  p('DROP PROCEDURE ' || v_sproc_prefix  || v_table_name || '_INSERT;');
   p('CREATE PROCEDURE ' || v_sproc_prefix  || v_table_name || '_INSERT (');
   OPEN c_cursor;
   
@@ -45,7 +42,7 @@ BEGIN
   END LOOP;
   
   CLOSE c_cursor;
-  
+  p('AS');
   p('BEGIN');
   
   
