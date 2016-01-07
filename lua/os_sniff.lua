63fb5f7aa75d4e6e116bc9053c72cdf987c2f6cc
Mark Sta Ana <booyaabooyaabooyaa@gmail.com>
2016-01-07 19:03:45 +0000

    Create os_sniff.lua

---
+++ lua/os_sniff.lua
@@ -0,0 +1,23 @@
+-- http://stackoverflow.com/a/326677/105282
+function os.capture(cmd, raw)
+  local f = assert(io.popen(cmd, 'r'))
+  local s = assert(f:read('*a'))
+  f:close()
+  if raw then return s end
+  s = string.gsub(s, '^%s+', '')
+  s = string.gsub(s, '%s+$', '')
+  s = string.gsub(s, '[\n\r]+', ' ')
+  return s
+end
+
+unix_test = os.capture("uname")
+os_name = ""
+if unix_test == "" then
+    os_name = "Windows"
+else 
+    os_name = unix_test
+end
+
+print(os_name) -- debugging purposes
+
+return os_name
