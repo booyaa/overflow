811822fc016487f2bf96140c0e45cb1d16969dc5
Mark Sta Ana <booyaabooyaabooyaa@gmail.com>
2016-03-19 17:10:32 +0000

    Create template.sql

---
+++ databases/oracle/scripts/template.sql
@@ -0,0 +1,32 @@
+CONNECT user_id/&password
+
+TIMING START "Release timing"
+
+COL ts NEW_VAL x
+SELECT TO_CHAR(sysdate, 'yyyymmdd-hh24miss') ts FROM DUAL;
+REM to hide query output
+CLEAR SCREEN
+
+spool release
+REM will create a spool called release.lst
+PROMPT Running release on &x
+
+REM example of a long running process and capturing the timing
+HOST SLEEP 5
+TIMING SHOW
+
+REM start logging of all output
+SET ECHO ON
+SET FEEDBACK ON
+
+SET DEFINE OFF
+PROMPT let &foo be &foo
+SET DEFINE ON
+
+TIMING OFF
+SET FEEDBACK OFF
+SET ECHO OFF
+PROMPT phew no more echoing of everything!
+
+SPOOL OFF
+EXIT
